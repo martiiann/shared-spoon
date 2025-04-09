b02b9f6789cb9055c29e659e56941b99f86e5de1
martiiann <martiian03817@icloud.com>
2025-04-09 12:59:37 +0100

    Add user registration and authentication system

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Recipe
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     recipes = Recipe.objects.all().order_by('-created_at')
     return render(request, 'recipes/index.html', {'recipes': recipes})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # redirect to login after registering
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
