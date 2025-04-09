10cbe3b119eda72b92cddd5d3df84f1b7bd5a0d0
martiiann <martiian03817@icloud.com>
2025-04-09 12:31:03 +0100

    Add index view and templates to display homepage

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Recipe
 
-# Create your views here.
+def index(request):
+    recipes = Recipe.objects.all().order_by('-created_at')
+    return render(request, 'recipes/index.html', {'recipes': recipes})
