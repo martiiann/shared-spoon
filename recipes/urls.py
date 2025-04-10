428e0e697dd5c80780c143e3c9cc6e1536367cf5
martiiann <martiian03817@icloud.com>
2025-04-10 13:37:16 +0100

    Add saved-by count with pluralization and minor template updates

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('my-recipes/', views.my_recipes, name='my_recipes'),
     path('favorite/<int:recipe_id>/', views.toggle_favorite, name='toggle_favorite'),
     path('favorites/', views.my_favorites, name='my_favorites'),
+    path('edit/<int:recipe_id>/', views.edit_recipe, name='edit_recipe'),
+    path('delete/<int:recipe_id>/', views.delete_recipe, name='delete_recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,20 +6,38 @@ from .forms import RecipeForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 def index(request):
     recipes = Recipe.objects.all().order_by('-created_at')
-    return render(request, 'recipes/index.html', {'recipes': recipes})
+    query = request.GET.get('q')
+    category = request.GET.get('category')
+
+    if query:
+        recipes = recipes.filter(title__icontains=query)
+
+    if category and category != 'all':
+        recipes = recipes.filter(category=category)
+
+    return render(request, 'recipes/index.html', {
+        'recipes': recipes,
+        'selected_category': category,
+        'query': query,
+    })
+
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Account created! You can now log in.")
             return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
 
 @login_required
 def add_recipe(request):
@@ -29,10 +47,12 @@ def add_recipe(request):
             recipe = form.save(commit=False)
             recipe.user = request.user
             recipe.save()
-            return redirect('index')
+            messages.success(request, "Recipe added successfully!")
+            return redirect('my_recipes')
     else:
         form = RecipeForm()
     return render(request, 'recipes/add_recipe.html', {'form': form})
+
 
 @login_required
 def my_recipes(request):
@@ -52,3 +72,31 @@ def toggle_favorite(request, recipe_id):
 def my_favorites(request):
     recipes = request.user.favorite_recipes.all().order_by('-created_at')
     return render(request, 'recipes/my_favorites.html', {'recipes': recipes})
+
+@login_required
+def edit_recipe(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    if recipe.user != request.user:
+        messages.error(request, "You can only edit your own recipes.")
+        return redirect('my_recipes')
+
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Recipe updated!")
+            return redirect('my_recipes')
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, 'recipes/edit_recipe.html', {'form': form})
+
+
+@login_required
+def delete_recipe(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    if recipe.user == request.user:
+        recipe.delete()
+        messages.success(request, "Recipe deleted!")
+    else:
+        messages.error(request, "You can only delete your own recipes.")
+    return redirect('my_recipes')
