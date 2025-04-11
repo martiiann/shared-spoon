2e2a8ff266c0c892cb172a1ad7b902a7813dcb4b
martiiann <martiian03817@icloud.com>
2025-04-11 13:11:01 +0100

    Fix logout error, clean navbar, and improve overall design readability

--- sharedspoon/settings.py
+++ sharedspoon/settings.py
@@ -125,3 +125,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Redirects after login/logout
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login'
