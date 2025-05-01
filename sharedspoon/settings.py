dec19a938de4e2d6b6f07ad1e76d18c11b2111ce
martiiann <martiian03817@icloud.com>
2025-05-01 23:12:59 +0100

    Secure SECRET_KEY using env.py and update .gitignore

--- sharedspoon/settings.py
+++ sharedspoon/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l5$@l(xq(t0y@lov-g=t_a6%76c#q!tth&*74c-rt&!ojf=*_f'
+import os
+if os.path.exists(os.path.join(BASE_DIR, "env.py")):
+    import env
+    
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
