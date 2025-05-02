cb09acdf0a9ca788cb27c52ea35a5850264134b0
martiiann <martiian03817@icloud.com>
2025-05-02 21:44:21 +0100

    Enable Whitenoise for static files on Heroku

--- sharedspoon/settings.py
+++ sharedspoon/settings.py
@@ -51,12 +51,14 @@ AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'sharedspoon.urls'
