c213549e395db731a396e4a35e2b72582374827d
martiiann <martiian03817@icloud.com>
2025-05-03 03:38:55 +0100

    Use Cloudinary as image storage

--- sharedspoon/settings.py
+++ sharedspoon/settings.py
@@ -158,8 +158,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Redirects after login/logout
 LOGIN_REDIRECT_URL = '/'
