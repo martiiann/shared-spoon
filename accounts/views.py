c8fa085030508447a0153db155e856696cd14a97
martiiann <martiian03817@icloud.com>
2025-05-05 02:47:53 +0100

    Add custom login/logout views with flash messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        messages.success(self.request, "You have successfully logged in.")
+        return super().form_valid(form)
+
+class CustomLogoutView(LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(self.request, "You have been logged out successfully.")
+        return super().dispatch(request, *args, **kwargs)
+
